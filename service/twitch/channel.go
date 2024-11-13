798466ef2ce879577a0a93900f2f31064b4d9d5e
Hugues GUILLEUS <ghugues@netc.fr>
2024-11-13 13:11:36 +0100

    Rename service/twitch/* fix inverted file name

--- service/twitch/channel.go
+++ service/twitch/channel.go
@@ -6,35 +6,71 @@ import (
 	"sniffle/tool"
 	"sniffle/tool/fetch"
 	"strconv"
+	"time"
 )
 
 const (
-	teamBodyBegin = `[{` +
-		`"operationName":"TeamLandingMemberList",` +
+	endpointURL = "https://gql.twitch.tv/gql"
+	clientID    = "kimne78kx3ncx6brgo4mv6wki5h1ko"
+
+	channelBodyBegin = `[` +
+		`{` +
+		`"operationName":"HomeShelfVideos",` +
 		`"variables":{` +
-		`"teamName":`
-	teamBodyEnd = `,` +
-		`"withLiveMembers":false,` +
-		`"withMembers":true` +
+		`"channelLogin":`
+	channelBodyMiddle = `,` +
+		`"first":1` +
 		`},` +
 		`"extensions":{` +
 		`"persistedQuery":{` +
 		`"version":1,` +
-		`"sha256Hash":"ee7d5bb7aeb195ac05164b6f306f1eb51db407c59f4398cbaa7901a3c3ba833d"` +
+		`"sha256Hash":"951c268434dc36a482c6f854215df953cf180fc2757f1e0e47aa9821258debf7"` +
 		`}` +
 		`}` +
-		`}]`
+		`},` +
+		`{` +
+		`"operationName":"ChannelRoot_AboutPanel",` +
+		`"variables":{` +
+		`"channelLogin":`
+	channelBodyEnd = `,` +
+		`"skipSchedule":true` +
+		`},` +
+		`"extensions":{` +
+		`"persistedQuery":{` +
+		`"version":1,` +
+		`"sha256Hash":"6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6"` +
+		`}` +
+		`}` +
+		`}` +
+		`]`
 )
 
-func Team(t *tool.Tool, id string) (list []*common.List) {
-	body := teamBodyBegin + strconv.Quote(id) + teamBodyEnd
-	dto := [1]struct {
+func Channel(t *tool.Tool, id string) *common.List {
+	qID := strconv.Quote(id)
+	body := channelBodyBegin + qID + channelBodyMiddle + qID + channelBodyEnd
+	list := &common.List{
+		Host: "twitch",
+		ID:   id,
+		URL:  "https://www.twitch.tv/" + id,
+		JSON: tool.FetchAll(t, fetch.Rs(http.MethodPost, endpointURL, body, "Client-ID", clientID)),
+	}
+
+	dto := []struct {
 		Data struct {
-			Team struct {
-				Members struct {
+			User struct {
+				DisplayName  string
+				Description  string
+				VideoShelves struct {
 					Edges []struct {
 						Node struct {
-							Login string
+							Items []struct {
+								ID          string
+								Title       string
+								PosterURL   string `json:"previewThumbnailURL"`
+								PublishedAt time.Time
+								Duration    int64 `json:"lengthSeconds"`
+								ViewCount   uint
+							}
 						}
 					}
 				}
@@ -45,9 +81,36 @@ func Team(t *tool.Tool, id string) (list []*common.List) {
 		return nil
 	}
 
-	for _, edge := range dto[0].Data.Team.Members.Edges {
-		list = append(list, Channel(t, edge.Node.Login))
+	for _, dto := range dto {
+		if title := dto.Data.User.DisplayName; title != "" {
+			list.Title = title
+		}
+		if d := dto.Data.User.Description; d != "" {
+			list.Description = common.Description(d)
+		}
+	}
+	for _, dto := range dto {
+		for _, edges := range dto.Data.User.VideoShelves.Edges {
+			for _, video := range edges.Node.Items {
+				poster, width, height := common.FetchPoster(t, video.PosterURL)
+
+				list.Items = append(list.Items, &common.Item{
+					Host:         "twitch",
+					ID:           video.ID,
+					URL:          "https://www.twitch.tv/videos/" + video.ID,
+					Title:        video.Title,
+					Author:       list.Title,
+					Published:    video.PublishedAt,
+					Duration:     time.Duration(video.Duration) * time.Second,
+					View:         video.ViewCount,
+					IsVideo:      true,
+					Poster:       poster,
+					PosterWidth:  width,
+					PosterHeight: height,
+				})
+			}
+		}
 	}
 
-	return
+	return list
 }
--- service/twitch/team.go
+++ service/twitch/team.go
@@ -6,71 +6,35 @@ import (
 	"sniffle/tool"
 	"sniffle/tool/fetch"
 	"strconv"
-	"time"
 )
 
 const (
-	endpointURL = "https://gql.twitch.tv/gql"
-	clientID    = "kimne78kx3ncx6brgo4mv6wki5h1ko"
-
-	channelBodyBegin = `[` +
-		`{` +
-		`"operationName":"HomeShelfVideos",` +
+	teamBodyBegin = `[{` +
+		`"operationName":"TeamLandingMemberList",` +
 		`"variables":{` +
-		`"channelLogin":`
-	channelBodyMiddle = `,` +
-		`"first":1` +
+		`"teamName":`
+	teamBodyEnd = `,` +
+		`"withLiveMembers":false,` +
+		`"withMembers":true` +
 		`},` +
 		`"extensions":{` +
 		`"persistedQuery":{` +
 		`"version":1,` +
-		`"sha256Hash":"951c268434dc36a482c6f854215df953cf180fc2757f1e0e47aa9821258debf7"` +
+		`"sha256Hash":"ee7d5bb7aeb195ac05164b6f306f1eb51db407c59f4398cbaa7901a3c3ba833d"` +
 		`}` +
 		`}` +
-		`},` +
-		`{` +
-		`"operationName":"ChannelRoot_AboutPanel",` +
-		`"variables":{` +
-		`"channelLogin":`
-	channelBodyEnd = `,` +
-		`"skipSchedule":true` +
-		`},` +
-		`"extensions":{` +
-		`"persistedQuery":{` +
-		`"version":1,` +
-		`"sha256Hash":"6089531acef6c09ece01b440c41978f4c8dc60cb4fa0124c9a9d3f896709b6c6"` +
-		`}` +
-		`}` +
-		`}` +
-		`]`
+		`}]`
 )
 
-func Channel(t *tool.Tool, id string) *common.List {
-	qID := strconv.Quote(id)
-	body := channelBodyBegin + qID + channelBodyMiddle + qID + channelBodyEnd
-	list := &common.List{
-		Host: "twitch",
-		ID:   id,
-		URL:  "https://www.twitch.tv/" + id,
-		JSON: tool.FetchAll(t, fetch.Rs(http.MethodPost, endpointURL, body, "Client-ID", clientID)),
-	}
-
-	dto := []struct {
+func Team(t *tool.Tool, id string) (list []*common.List) {
+	body := teamBodyBegin + strconv.Quote(id) + teamBodyEnd
+	dto := [1]struct {
 		Data struct {
-			User struct {
-				DisplayName  string
-				Description  string
-				VideoShelves struct {
+			Team struct {
+				Members struct {
 					Edges []struct {
 						Node struct {
-							Items []struct {
-								ID          string
-								Title       string
-								PosterURL   string `json:"previewThumbnailURL"`
-								PublishedAt time.Time
-								Duration    int64 `json:"lengthSeconds"`
-								ViewCount   uint
-							}
+							Login string
 						}
 					}
 				}
@@ -81,36 +45,9 @@ func Channel(t *tool.Tool, id string) *common.List {
 		return nil
 	}
 
-	for _, dto := range dto {
-		if title := dto.Data.User.DisplayName; title != "" {
-			list.Title = title
-		}
-		if d := dto.Data.User.Description; d != "" {
-			list.Description = common.Description(d)
-		}
-	}
-	for _, dto := range dto {
-		for _, edges := range dto.Data.User.VideoShelves.Edges {
-			for _, video := range edges.Node.Items {
-				poster, width, height := common.FetchPoster(t, video.PosterURL)
-
-				list.Items = append(list.Items, &common.Item{
-					Host:         "twitch",
-					ID:           video.ID,
-					URL:          "https://www.twitch.tv/videos/" + video.ID,
-					Title:        video.Title,
-					Author:       list.Title,
-					Published:    video.PublishedAt,
-					Duration:     time.Duration(video.Duration) * time.Second,
-					View:         video.ViewCount,
-					IsVideo:      true,
-					Poster:       poster,
-					PosterWidth:  width,
-					PosterHeight: height,
-				})
-			}
-		}
+	for _, edge := range dto[0].Data.Team.Members.Edges {
+		list = append(list, Channel(t, edge.Node.Login))
 	}
 
-	return list
+	return
 }
